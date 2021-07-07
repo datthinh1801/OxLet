c40447961ebd779e2bbff801e7b424fb7187ddc2
dat._.thinh <44528004+datthinh1801@users.noreply.github.com>
2021-07-07 21:13:53 +0700

    Modularize the crawler

--- oxlet.py
+++ oxlet.py
@@ -2,6 +2,8 @@ from subprocess import check_output
 from bs4 import BeautifulSoup
 import argparse
 
+BASE_URL = "https://www.oxfordlearnersdictionaries.com/definition/english/"
+
 
 def create_parser():
     parser = argparse.ArgumentParser()
@@ -56,11 +58,59 @@ def preprocess_words(words):
     return list(map(lambda word: "-".join(word.split(" ")), words))
 
 
+def crawl_resource(word) -> str:
+    page = check_output(f"curl -s -L {BASE_URL + word}", shell=True)
+    soup = BeautifulSoup(page, "html.parser")
+    sense = soup.find(class_="sense")
+
+    result = ""
+
+    # extract headword
+    try:
+        headword = soup.find(class_="headword")
+        result += headword.text
+    except:
+        return word + "\n\n"
+
+    try:
+        # extract phonetic
+        phon = soup.find(class_="phon")
+        result += f"\n({phon.text})"
+    except:
+        pass
+
+    result += "|"
+
+    # extract word form
+    try:
+        word_form = soup.find(class_="pos")
+        result += f"({word_form.text}) "
+    except:
+        pass
+
+    # extract definition
+    try:
+        definition = sense.find(class_="def")
+        result += definition.text
+    except:
+        return word + "\n\n"
+
+    try:
+        # extract example
+        example = sense.find(class_="examples").find_next("li")
+        result += "\ne.g. " + example.text
+    except:
+        pass
+
+    # append delimiter between 2 words
+    result += "\n\n"
+    return result
+
+
 if __name__ == "__main__":
     args = create_parser()
     words = get_word_list(args)
     words = preprocess_words(words)
-    base_url = "https://www.oxfordlearnersdictionaries.com/definition/english/"
 
     # empty the outfile first
     with open(args.outfile, "w") as f:
@@ -68,50 +118,6 @@ if __name__ == "__main__":
     with open(args.outfile, "ab") as outfile:
         for word in words:
             print(f"Processing {word}...")
-            page = check_output(f"curl -s -L {base_url + word}", shell=True)
-            soup = BeautifulSoup(page, "html.parser")
-            sense = soup.find(class_="sense")
-
-            result = ""
-
-            # extract headword
-            try:
-                headword = soup.find(class_="headword")
-                result += headword.text
-            except:
-                print("\tNo headword was found! Skip this word...")
-                outfile.write(f"{word}\n\n".encode())
-                continue
-
-            try:
-                # extract phonetic
-                phon = soup.find(class_="phon")
-                result += f"\n({phon.text})"
-            except:
-                print("\tNo phonetic was found!")
-
-            result += "|"
-
-            # extract word form
-            try:
-                word_form = soup.find(class_="pos")
-                result += f"({word_form.text}) "
-            except:
-                print("\tNo word form was found!")
-
-            # extract definition
-            definition = sense.find(class_="def")
-            result += definition.text + "\n"
-
-            try:
-                # extract example
-                example = sense.find(class_="examples").find_next("li")
-                result += "e.g. " + example.text
-            except:
-                print("\tNo example was found!")
-
-            # append delimiter between 2 words
-            result += "\n\n"
-
+            result = crawl_resource(word)
             # append the result to file
             outfile.write(result.encode())
