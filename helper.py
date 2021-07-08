84ee28165b939c73a83aab25533fd38b205611ce
dat._.thinh <44528004+datthinh1801@users.noreply.github.com>
2021-07-08 14:33:26 +0700

    Successfully migrate CLI to web interface

---
+++ helper.py
@@ -0,0 +1,58 @@
+from subprocess import check_output
+from bs4 import BeautifulSoup
+
+
+BASE_URL = "https://www.oxfordlearnersdictionaries.com/definition/english/"
+
+
+def preprocess_words(words):
+    return list(map(lambda word: "-".join(word.split(" ")), words))
+
+
+def crawl_resource(word) -> str:
+    page = check_output(f"curl -s -L {BASE_URL + word}", shell=True)
+    soup = BeautifulSoup(page, "html.parser")
+    sense = soup.find(class_="sense")
+
+    result = ""
+
+    # extract headword
+    try:
+        headword = soup.find(class_="headword")
+        result += headword.text
+    except:
+        return word + "\n\n"
+
+    try:
+        # extract phonetic
+        phon = soup.find(class_="phon")
+        result += f"\n({phon.text})"
+    except:
+        pass
+
+    result += "|"
+
+    # extract word form
+    try:
+        word_form = soup.find(class_="pos")
+        result += f"({word_form.text}) "
+    except:
+        pass
+
+    # extract definition
+    try:
+        definition = sense.find(class_="def")
+        result += definition.text
+    except:
+        return word + "\n\n"
+
+    try:
+        # extract example
+        example = sense.find(class_="examples").find_next("li")
+        result += "\ne.g. " + example.text
+    except:
+        pass
+
+    # append delimiter between 2 words
+    result += "\n\n"
+    return result
--- oxlet-web.py
+++ oxlet-web.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-from oxlet import preprocess_words, crawl_resource
+from helper import preprocess_words, crawl_resource
 
 
 app = Flask(__name__, static_folder="templates/images")
@@ -11,10 +11,10 @@ def index():
     if request.method != "POST":
         return render_template("index.html")
 
-    words = request.form["new-words"]
-    words = words.splitlines()
+    new_words = request.form["new-words"]
+    words = new_words.splitlines()
     result = "".join(list(map(crawl_resource, words)))
-    return render_template("result.html", result=result)
+    return render_template("index.html", new_words=new_words, result=result)
 
 
 if __name__ == "__main__":
