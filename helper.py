b63b35d1bbc82d7ca8a794d66b7a73a1ba1e043a
Nguyen Dat Thinh <44528004+datthinh1801@users.noreply.github.com>
2021-07-30 14:54:26 +0700

    Support Cambridge dictionary

--- helper.py
+++ helper.py
@@ -3,9 +3,30 @@ import requests
 from bs4 import BeautifulSoup
 
 
-BASE_URL = "https://www.oxfordlearnersdictionaries.com/definition/english/"
+BASE_URL = {"oxford": "https://www.oxfordlearnersdictionaries.com/definition/english/",
+            "cambridge": "https://dictionary.cambridge.org/dictionary/english/"}
 HEADERS = requests.utils.default_headers()
 HEADERS.update({"User-Agent": "Edge"})
+DICT_PROFILES = {
+    "oxford": {
+        "dict_type": "oxford",
+        "hw_class": "headword",
+        "phon_class": "phon",
+        "wf_class": "pos",
+        "sense_class": "sense",
+        "def_class": "def",
+        "ex_class": "x"
+    },
+    "cambridge": {
+        "dict_type": "cambridge",
+        "hw_class": "hw dhw",
+        "phon_class": "ipa dipa lpr-2 lpl-1",
+        "wf_class": "pos dpos",
+        "sense_class": "ddef_block",
+        "def_class": "def ddef_d db",
+        "ex_class": "eg deg"
+    }
+}
 
 NUM_THREADS = 10
 
@@ -21,53 +42,62 @@ def preprocess_words(words):
     return list(map(lambda word: "-".join(word.split(" ")), words))
 
 
-def crawl_resource(word) -> str:
-    """Get the web page of the word and parse it."""
-    page = requests.get(BASE_URL + word, headers=HEADERS).text
+def parse_page(word, dict_type, hw_class, phon_class, wf_class, sense_class, def_class, ex_class) -> str:
+    """Crawl the source code and parse elements via given classes."""
+    page = requests.get(BASE_URL[dict_type] + word, headers=HEADERS).text
     soup = BeautifulSoup(page, "html.parser")
-    sense = soup.find(class_="sense")
-
-    result = ""
 
     # extract headword
     try:
-        headword = soup.find(class_="headword")
-        result += headword.text
+        headword = soup.find(class_=hw_class)
+        result = headword.text
     except:
-        return word + "\n\n"
+        return None
 
+    # extract phonetic
     try:
         # extract phonetic
-        phon = soup.find_all(class_="phon")[1]
+        phon = soup.find_all(class_=phon_class)[1]
         result += f"\n({phon.text})"
     except:
         pass
 
-    result += "|"
-
     # extract word form
     try:
-        word_form = soup.find(class_="pos")
-        result += f"({word_form.text}) "
+        word_form = soup.find(class_=wf_class)
+        result += f"|({word_form.text}) "
     except:
         pass
 
+    sense = soup.find(class_=sense_class)
     # extract definition
     try:
-        definition = sense.find(class_="def")
-        result += definition.text
+        definition = sense.find(class_=def_class)
+        if dict_type == "cambridge":
+            result += definition.text.replace(':', '')
+        else:
+            result += definition.text
+        print(result)
     except:
-        return word + "\n\n"
+        return None
 
+    # extract example
     try:
-        # extract example
-        example = sense.find(class_="examples").find_next("li")
+        example = sense.find(class_=ex_class)
         result += "\ne.g. " + example.text
     except:
         pass
 
     # append delimiter between 2 words
     result += "\n\n"
+    return result
+
+
+def crawl_resource(word) -> str:
+    """Get the web page of the word and parse it."""
+    result = parse_page(word, **DICT_PROFILES['oxford'])
+    if result is None:
+        result = parse_page(word, **DICT_PROFILES['cambridge'])
     return result
 
 
