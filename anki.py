fd4130a75e395265fde9633ed010a58c9ef2284d
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2021-10-22 21:56:53 +0700

    Update model templates

--- anki.py
+++ anki.py
@@ -58,6 +58,7 @@ async def check_model_exist(session: ClientSession) -> bool:
 
 async def create_new_model(session: ClientSession):
     if await check_model_exist(session):
+        await update_model_template(session)
         return None
 
     new_model = {
@@ -71,23 +72,50 @@ async def create_new_model(session: ClientSession):
                 {
                     "Name": "Basic type",
                     "Front": "{{Word}}<br>{{Phonetic}}",
-                    "Back": "{{Word}}<br>{{Phonetic}}<hr id=answer>{{Definition}}"
+                    "Back": "{{Word}}<br>{{Phonetic}}<hr id=answer>{{Word form}}<br>{{Definition}}"
                 },
                 {
                     "Name": "Basic reverse type",
                     "Front": "{{Definition}}",
-                    "Back": "{{Definition}}<hr id=answer>{{Word}}<br>{{Phonetic}}"
+                    "Back": "{{Word form}}<br>{{Definition}}<hr id=answer>{{Word}}<br>{{Phonetic}}"
                 },
                 {
                     "Name": "Listening type",
                     "Front": "{{Phonetic}}",
-                    "Back": "{{Word}}<br>{{Phonetic}}<hr id=answer>{{Definition}}"
+                    "Back": "{{Word}}<br>{{Phonetic}}<hr id=answer>{{Word form}}<br>{{Definition}}"
                 }
             ]
         }
     }
     # Sometimes, a ConnectionResetError occurs
     return await send_to_anki(session, new_model)
+
+
+async def update_model_template(session: ClientSession):
+    new_model_template = {
+        'action': 'updateModelTemplates',
+        'version': 6,
+        'params': {
+            'model': {
+                'name': 'English (by datthinh1801)',
+                'templates': {
+                    "Basic type": {
+                        "Front": "{{Word}}<br>{{Phonetic}}",
+                        "Back": "{{Word}}<br>{{Phonetic}}<hr id=answer>{{Word form}}<br>{{Definition}}"
+                    },
+                    "Basic reverse type": {
+                        "Front": "{{Definition}}",
+                        "Back": "{{Word form}}<br>{{Definition}}<hr id=answer>{{Word}}<br>{{Phonetic}}"
+                    },
+                    "Listening type": {
+                        "Front": "{{Phonetic}}",
+                        "Back": "{{Word}}<br>{{Phonetic}}<hr id=answer>{{Word form}}<br>{{Definition}}"
+                    }
+                }
+            }
+        }
+    }
+    return await send_to_anki(session, new_model_template)
 
 
 async def run(wordlist: list):
